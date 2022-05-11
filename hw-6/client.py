9ff940b6db1b6b97dfe59f912dec9d7db86beea1
Eremey Remzin <hungry.green.bee@gmail.com>
2022-05-11 17:50:30 +0300

    client function -> client class

--- hw-6/client.py
+++ hw-6/client.py
@@ -3,7 +3,7 @@
 import socket
 import threading
 
-from config import server_port, urls_path
+from config import server_port
 
 from utils import client_args_parser
 
@@ -15,33 +15,39 @@ def get_urls(path):
     return urls.split()
 
 
-def client_thread():
-    """thread for requests"""
-    urls = get_urls(urls_path)
-    for url in urls:
-        sock = socket.socket()
-        sock.connect(("", server_port))
-        sock.sendall(url.encode())
-        data = sock.recv(1024)
-        print(f"{url}: {data.decode('utf8')}")
+class Client:
+    """client implementation class"""
+    def __init__(self, numb_of_threads, urls_path):
+        self.numb_of_threads = numb_of_threads
+        self.urls_path = urls_path
 
+    def start(self):
+        """main thread"""
+        threads = [
+            threading.Thread(target=self.client_thread,
+                             name=f"clt_th_{i}", args=())
+            for i in range(self.numb_of_threads)
+        ]
 
-def client(numb_of_threads):
-    """main thread"""
-    threads = [
-        threading.Thread(target=client_thread, name=f"clt_th_{i}", args=())
-        for i in range(numb_of_threads)
-    ]
+        for thread in threads:
+            thread.start()
 
-    for thread in threads:
-        thread.start()
+        for thread in threads:
+            thread.join()
 
-    for thread in threads:
-        thread.join()
+    def client_thread(self):
+        """thread for requests"""
+        urls = get_urls(self.urls_path)
+        for url in urls:
+            sock = socket.socket()
+            sock.connect(("", server_port))
+            sock.sendall(url.encode())
+            data = sock.recv(1024)
+            print(f"{url}: {data.decode('utf8')}")
 
 
 if __name__ == "__main__":
     args = client_args_parser()
 
-    urls_path = args.p
-    client(args.m)
+    client = Client(args.m, args.p)
+    client.start()
--- hw-6/config.py
+++ hw-6/config.py
@@ -1,4 +1,2 @@
 server_port = 15025
 
-urls_path = "urls.asdtxt"
-
